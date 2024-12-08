98a492f39d066ed49375c6e4678587b5e4d6e667
oleksandrkhmil <oleksandr.khmil@sumup.com>
2024-12-08 10:33:43 +0200

    feat(get post by id): added get /api/v1/posts/{id} endpoint

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,6 +7,7 @@ func NewServer(port string, blogHandler BlogHandler) *http.Server {
 
 	mux.HandleFunc("GET /api/v1/posts", blogHandler.GetAll)
 	mux.HandleFunc("POST /api/v1/posts", blogHandler.Create)
+	mux.HandleFunc("GET /api/v1/posts/{id}", blogHandler.GetByID)
 
 	return &http.Server{
 		Addr:    ":" + port,
